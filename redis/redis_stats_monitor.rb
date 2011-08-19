8f58dac2e951fe0bf88aad1d3feccf23c5b305ab
Tim Meighen <tim.meighen@pulseenergy.com>
2011-08-19 13:06:41 -0700

    Allow configuration to be provided from a file

--- redis/redis_stats_monitor.rb
+++ redis/redis_stats_monitor.rb
@@ -1,7 +1,10 @@
-class HealthCheck < Scout::Plugin
-  needs 'redis'
+class RedisStatsMonitor < Scout::Plugin
+  needs 'redis', 'yaml'
 
   OPTIONS=<<-EOS
+    config_file:
+      name: Config File
+      notes: Configuration file on each server from which to load all other configuration values. Providing a config file value overrides any other configuration settings and can be used as an alternative to configuring the plugin in the Scout interface.
     redis_host:
       default: localhost
       name: Redis Host
@@ -29,31 +32,57 @@ class HealthCheck < Scout::Plugin
       notes: Granularity at which rates are calculated ('second', 'minute')
   EOS
 
+
+  def configure_from_options
+    @redis = Redis.new :port => option(:redis_port),
+                       :db => option(:redis_database),
+                       :password => option(:redis_password),
+                       :host => option(:redis_host)
+
+    @value_key_prefix = option(:value_key_prefix).to_s
+    @rate_key_prefix = option(:rate_key_prefix).to_s
+    @granularity = option(:rate_granularity).to_s == 'second' ? :sec : :minute
+  end
+
+  def configure_from_file(config_file)
+    config = YAML::load(File.open(config_file, "r"))
+
+    redis_config = config["redis"]
+    @redis = Redis.new :port => redis_config["port"],
+                       :db => redis_config["database"],
+                       :password => redis_config["password"],
+                       :host => redis_config["host"]
+
+    prefixes = config["key_prefixes"]
+    @value_key_prefix = prefixes["value"].to_s
+    @rate_key_prefix = prefixes["rate"].to_s
+    @granularity = config["rate_granularity"].to_s
+  end
+
   def build_report
-    redis = Redis.new :port     => option(:redis_port),
-                      :db       => option(:redis_database),
-                      :password => option(:redis_password),
-                      :host     => option(:redis_host)
+    config_file = option(:config_file).to_s
 
-    value_key_prefix = option(:value_key_prefix).to_s
-    rate_key_prefix = option(:rate_key_prefix).to_s
+    if config_file.empty?
+      configure_from_options()
+    else
+      configure_from_file(config_file)
+    end
 
-    value_keys = redis.keys(value_key_prefix + "*")
+    value_keys = @redis.keys(@value_key_prefix + "*")
 
     value_keys.each do |key|
-      value = redis.get(key).to_i
+      value = @redis.get(key).to_i
       name = key
-      name[value_key_prefix] = ""
+      name[@value_key_prefix] = ""
       report(name => value)
     end
 
-    granularity = option(:rate_granularity) == 'second' ? :sec : :minute
-    rate_keys = redis.keys(rate_key_prefix + "*")
+    rate_keys = @redis.keys(@rate_key_prefix + "*")
     rate_keys.each do |key|
-      value = redis.get(key).to_i
+      value = @redis.get(key).to_i
       name = key
-      name[rate_key_prefix] = ""
-      counter(name, value, :per => granularity)
+      name[@rate_key_prefix] = ""
+      counter(name, value, :per => @granularity)
     end
   end
 end
